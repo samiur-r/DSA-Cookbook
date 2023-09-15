f5eb51c4df27a252cd36d3042b743e1ec11380fa
“samiur-r” <samiurrahman.dev@gmail.com>
2023-09-15 23:54:49 +0600

    feat: add rod-cutting algo

--- Dynamic Programming/rod_cutting.cpp
+++ Dynamic Programming/rod_cutting.cpp
@@ -7,17 +7,22 @@ int max(int a, int b) {
 }
 
 int rodCutting(int price[], int n) {
-    if (n <= 0)
+    if (n <= 0) {
         return 0;
-
-    int maxVal = -1;
+    }
+    
+    int dp[n + 1];
+    dp[0] = 0;
 
     for (int i = 1; i <= n; i++) {
-        int currentVal = price[i - 1] + rodCutting(price, n - i);
-        maxVal = max(maxVal, currentVal);
+        int maxVal = -1;
+        for (int j = 0; j < i; j++) {
+            maxVal = max(maxVal, price[j] + dp[i - j - 1]);
+        }
+        dp[i] = maxVal;
     }
 
-    return maxVal;
+    return dp[n];
 }
 
 int main() {
